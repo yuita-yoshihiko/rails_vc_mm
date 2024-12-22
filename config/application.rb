5d5a2f3bb26fcd11ff5c45d6113f015d813f0f82
yuita-yoshihiko <soup998211@gmail.com>
2024-12-22 23:15:56 +0900

    packwerk検証の下準備

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.paths.add 'app/packages', glob: '*/{*,*/concerns}', eager_load: true # 追記
   end
 end
--- packages/rails_shims/controllers/application_controller.rb
+++ packages/rails_shims/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  append_view_path(Dir.glob(Rails.root.join('app/packages/*/views'))) # 追記
   allow_browser versions: :modern
 end
